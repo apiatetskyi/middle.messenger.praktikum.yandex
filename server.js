171d4ad7cb13eed654e7c708a56811f888b55129
a.piatetskyi <a.piatetskyi@gmail.com>
2022-01-24 12:17:28 +0200

    Fix review comments

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
